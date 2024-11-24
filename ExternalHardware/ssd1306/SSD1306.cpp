2969bb1cbdbbd86bca6967264746f9df4d2b57e0
Dmitry Krasnyansky <k.dimonych@gmail.com>
2024-11-25 01:26:08 +0200

    fix: the "Fundamental Command Table" was implemented

--- ExternalHardware/ssd1306/SSD1306.cpp
+++ ExternalHardware/ssd1306/SSD1306.cpp
@@ -9,4 +9,91 @@ namespace
 {
 }
 
+CSsd1306::CSsd1306( AbstractPlatform::IAbstractI2CBus& aI2CBus,
+                    std::uint8_t aDeviceAddress ) NOEXCEPT : iI2CBus{ aI2CBus },
+                                                             iDeviceAddress{ aDeviceAddress }
+{
+}
+
+template < typename taRegisterType >
+AbstractPlatform::TErrorCode
+CSsd1306::ReadRegister( std::uint8_t aRegisterAddress, taRegisterType& aRegisterValue ) NOEXCEPT
+{
+    const auto result
+        = aRegisterAddress == iLastRegisterAddress
+              ? iI2CBus.ReadLastRegisterRaw( iDeviceAddress, aRegisterValue )
+              : iI2CBus.ReadRegisterRaw( iDeviceAddress, aRegisterAddress, aRegisterValue );
+    if ( result )
+    {
+        iLastRegisterAddress = aRegisterAddress;
+        aRegisterValue = ChangeEndian( aRegisterValue );
+        return AbstractPlatform::KOk;
+    }
+    return AbstractPlatform::KGenericError;
+}
+
+template < typename taRegisterType >
+AbstractPlatform::TErrorCode
+CSsd1306::WriteRegister( std::uint8_t aRegisterAddress, taRegisterType aRegisterValue ) NOEXCEPT
+{
+    aRegisterValue = ChangeEndian( aRegisterValue );
+    const auto result
+        = iI2CBus.WriteRegisterRaw( iDeviceAddress, aRegisterAddress, aRegisterValue );
+    if ( result )
+    {
+        iLastRegisterAddress = aRegisterAddress;
+        return AbstractPlatform::KOk;
+    }
+    return AbstractPlatform::KGenericError;
+}
+
+AbstractPlatform::TErrorCode
+CSsd1306::SendCommand( uint8_t aCommand, bool aNoStop ) NOEXCEPT
+{
+    // I2C write process expects a control byte followed by data
+    // this "data" can be a command or data to follow up a command
+    // Co = 1, D/C = 0 => the driver expects a command
+    constexpr std::uint8_t controlByte = 0x80;
+    if ( iI2CBus.WriteRegisterRaw( iDeviceAddress, controlByte, aCommand ) )
+    {
+        return AbstractPlatform::KOk;
+    }
+    return AbstractPlatform::KGenericError;
+}
+
+AbstractPlatform::TErrorCode
+CSsd1306::SendCommands( const uint8_t* aCommands, size_t aCommandsNumber ) NOEXCEPT
+{
+    AbstractPlatform::TErrorCode result = AbstractPlatform::KOk;
+    for ( size_t i = 0; i < aCommandsNumber; i++ )
+    {
+        result = SendCommand( aCommands[ i ], ( i + 1 ) < aCommandsNumber );
+        if ( result != AbstractPlatform::KOk )
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+AbstractPlatform::TErrorCode
+CSsd1306::SendBuffer( uint8_t aBuffer[], size_t aBufferSize, bool aAutoReleaseMemory ) NOEXCEPT
+{
+    // Control byte
+    constexpr std::uint8_t controlByte = 0x40;
+
+    iDataBuffer.reserve( sizeof( controlByte ) + aBufferSize );
+    iDataBuffer[ 0 ] = controlByte;
+    iDataBuffer.insert( iDataBuffer.end( ), aBuffer, aBuffer + aBufferSize );
+
+    const auto result
+        = iI2CBus.Write( iDeviceAddress, iDataBuffer.data( ), iDataBuffer.size( ), false );
+
+    if ( aAutoReleaseMemory )
+    {
+        iDataBuffer.resize( 0u );
+    }
+    return result == iDataBuffer.size( ) ? AbstractPlatform::KOk : AbstractPlatform::KGenericError;
+}
+
 }  // namespace ExternalHardware
--- ExternalHardware/ssd1306/SSD1306.hpp
+++ ExternalHardware/ssd1306/SSD1306.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <cstring>
+#include <vector>
 #include <AbstractPlatform/common/Platform.hpp>
 #include <AbstractPlatform/common/ErrorCode.hpp>
 #include <AbstractPlatform/common/PlatformLiteral.hpp>
@@ -13,8 +14,17 @@ namespace ExternalHardware
 class CSsd1306
 {
 public:
-    static constexpr std::uint8_t KDefaultAddress = 0x40;  // A0 pulled to GND
-    static constexpr std::uint8_t KVSAddress = 0x41;       // A0 pulled to VS
+    static constexpr std::uint8_t KDefaultAddress = 0x3C;      // SA0 pulled to GND
+    static constexpr std::uint8_t KAlternativeAddress = 0x3D;  // SA0 pulled to VS
+
+    // Fundamental Command Table
+    static constexpr std::uint8_t KCmdDisplayOn = 0xAF;  // Default
+    static constexpr std::uint8_t KCmdDisplayOff = 0xAE;
+    static constexpr std::uint8_t KCmdEnableRamDisplay = 0xA4;  // Default
+    static constexpr std::uint8_t KCmdDisableRamDisplay = 0xA5;
+    static constexpr std::uint8_t KCmdNormalDisplay = 0xA6;  // Default
+    static constexpr std::uint8_t KCmdInverseDisplay = 0xA7;
+    static constexpr std::uint8_t KCmdContrast = 0x81;
 
     CSsd1306( AbstractPlatform::IAbstractI2CBus& aI2CBus,
               std::uint8_t aDeviceAddress = KDefaultAddress ) NOEXCEPT;
@@ -29,28 +39,74 @@ public:
     inline AbstractPlatform::TErrorCode
     Reset( ) NOEXCEPT
     {
-        // TODO: implement
-        return AbstractPlatform::KOk;
+        CConfig config;
+        return SetConfig( config ) == AbstractPlatform::KOk;
     }
 
     inline AbstractPlatform::TErrorCode
     Reset( CConfig aConfig ) NOEXCEPT
     {
-        // TODO: implement
-        return AbstractPlatform::KOk;
+        auto result = SetConfig( aConfig ) == AbstractPlatform::KOk;
+        if ( result == AbstractPlatform::KOk )
+        {
+            return SetConfig( aConfig );
+        }
+        return result;
     }
 
     AbstractPlatform::TErrorCode GetConfig( CConfig& aConfig ) NOEXCEPT;
     AbstractPlatform::TErrorCode SetConfig( const CConfig& aConfig ) NOEXCEPT;
 
+    inline AbstractPlatform::TErrorCode
+    EnableRamDisplay( bool aEnable ) NOEXCEPT
+    {
+        return SendCommand( aEnable ? KCmdEnableRamDisplay : KCmdDisableRamDisplay );
+    }
+
+    inline AbstractPlatform::TErrorCode
+    DisplayEnable( bool aOn ) NOEXCEPT
+    {
+        // The KCmdDisplayOff switches the display to the sleep mode
+        return SendCommand( aOn ? KCmdDisplayOn : KCmdDisplayOff );
+    }
+
+    inline AbstractPlatform::TErrorCode
+    InverseDisplay( bool aInverse = false ) NOEXCEPT
+    {
+        return SendCommand( aInverse ? KCmdInverseDisplay : KCmdNormalDisplay );
+    }
+
+    inline AbstractPlatform::TErrorCode
+    SetContrast( std::uint8_t aContrast = 0x7f ) NOEXCEPT
+    {
+        const std::uint8_t commands[] = { KCmdContrast, aContrast };
+        return SendCommands( commands, sizeof( commands ) );
+    }
+
 #ifdef __EXCEPTIONS
 
 #endif
 
+    AbstractPlatform::TErrorCode SendCommand( uint8_t aCommand, bool aNoStop = false ) NOEXCEPT;
+    AbstractPlatform::TErrorCode SendCommands( const uint8_t* aCommands,
+                                               size_t aCommandsNumber ) NOEXCEPT;
+
 private:
+    template < typename taRegisterType >
+    AbstractPlatform::TErrorCode ReadRegister( std::uint8_t aReg,
+                                               taRegisterType& aRegisterValue ) NOEXCEPT;
+    template < typename taRegisterType >
+    AbstractPlatform::TErrorCode WriteRegister( std::uint8_t aReg,
+                                                taRegisterType aRegisterValue ) NOEXCEPT;
+
+    AbstractPlatform::TErrorCode SendBuffer( uint8_t aBuffer[],
+                                             size_t aBufferSize,
+                                             bool aAutoReleaseMemory = false ) NOEXCEPT;
+
     /* data */
     AbstractPlatform::IAbstractI2CBus& iI2CBus;
     const std::uint8_t iDeviceAddress;
+    std::vector< std::uint8_t > iDataBuffer;
 };
 
 }  // namespace ExternalHardware
