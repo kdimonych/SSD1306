08a11318d8f5a31b5d2d44fc505a8b0d71d001b0
Dmitry Krasnyansky <k.dimonych@gmail.com>
2024-11-30 01:07:16 +0200

    fix: added new commands

--- ExternalHardware/ssd1306/SSD1306.hpp
+++ ExternalHardware/ssd1306/SSD1306.hpp
@@ -88,8 +88,8 @@ public:
         constexpr std::uint8_t KDummyByte = 0x00;
         constexpr std::uint8_t KDummyEndByte = 0xff;
 
-        assert( aStartPage & 0x07 == aStartPage );
-        assert( aEndPage & 0x07 == aEndPage );
+        assert( aStartPage <= 0x07 );
+        assert( aEndPage <= 0x07 );
         assert( aStartPage <= aEndPage );
 
         const std::uint8_t commands[]
@@ -116,12 +116,10 @@ public:
         constexpr std::uint8_t KScrollDirectionRight = 0x2A;
         constexpr std::uint8_t KDummyByte = 0x00;
 
-        assert( aStartPage & 0x07 == aStartPage );
-        assert( aEndPage & 0x07 == aEndPage );
+        assert( aStartPage <= 0x07 );
+        assert( aEndPage <= 0x07 );
         assert( aStartPage <= aEndPage );
-        assert( static_cast< std::uint8_t >( aScrollStepInterval )
-                & 0x07 == static_cast< std::uint8_t >( aScrollStepInterval ) );
-        assert( aVerticalScrollOffset & 0x3F == aVerticalScrollOffset );
+        assert( aVerticalScrollOffset <= 0x3F );
 
         const std::uint8_t commands[]
             = { aScrollDirectionLeft ? KScrollDirectionLeft : KScrollDirectionRight,
@@ -154,8 +152,8 @@ public:
         using namespace AbstractPlatform;
         constexpr std::uint8_t KSetVerticalScrollArea = 0xA3;
 
-        assert( aNumberOfRowsInTopFixedArea & 0x3F == aNumberOfRowsInTopFixedArea );
-        assert( aNumberOfRowsInScrollArea & 0x7F == aNumberOfRowsInScrollArea );
+        assert( aNumberOfRowsInTopFixedArea <= 0x3F );
+        assert( aNumberOfRowsInScrollArea <= 0x7F );
 
         const std::uint8_t commands[] = {
             KSetVerticalScrollArea,
@@ -169,14 +167,14 @@ public:
     // Addressing Setting Command
     TErrorCode inline SetLowerColumnStartAddress( std::uint8_t aStartAddress ) NOEXCEPT
     {
-        assert( aStartAddress & 0x0F == aStartAddress );
+        assert( aStartAddress <= 0x0F );
         return SendCommand( static_cast< std::uint8_t >( aStartAddress & 0x0F ) );
     }
 
     TErrorCode inline SetHigherColumnStartAddress( std::uint8_t aStartAddress ) NOEXCEPT
     {
         constexpr std::uint8_t KCmdCSetLowerColumnStartAddress = 0x10;
-        assert( aStartAddress & 0x0F == aStartAddress );
+        assert( aStartAddress <= 0x0F );
         return SendCommand(
             static_cast< std::uint8_t >( KCmdCSetLowerColumnStartAddress | aStartAddress & 0x0F ) );
     }
@@ -204,8 +202,8 @@ public:
         using namespace AbstractPlatform;
         constexpr std::uint8_t KCmdSetColumnAddress = 0x21;
 
-        assert( aColumnStartAddress & 0x7F == aColumnStartAddress );
-        assert( aColumnEndAddress & 0x7F == aColumnEndAddress );
+        assert( aColumnStartAddress <= 0x7F );
+        assert( aColumnEndAddress <= 0x7F );
 
         const std::uint8_t commands[] = {
             KCmdSetColumnAddress,
@@ -222,8 +220,8 @@ public:
         using namespace AbstractPlatform;
         constexpr std::uint8_t KCmdSetColumnAddress = 0x22;
 
-        assert( aPageStartAddress & 0x07 == aPageStartAddress );
-        assert( aPageEndAddress & 0x07 == aPageEndAddress );
+        assert( aPageStartAddress <= 0x07 );
+        assert( aPageEndAddress <= 0x07 );
 
         const std::uint8_t commands[] = {
             KCmdSetColumnAddress,
@@ -237,7 +235,7 @@ public:
     TErrorCode inline SetPageStartAddress( std::uint8_t aPageStartAddress ) NOEXCEPT
     {
         constexpr std::uint8_t KCmdPageStartAddress = 0xB0;
-        assert( aPageStartAddress & 0x07 == aPageStartAddress );
+        assert( aPageStartAddress <= 0x07 );
         return SendCommand(
             static_cast< std::uint8_t >( KCmdPageStartAddress | aPageStartAddress & 0x07 ) );
     }
@@ -246,7 +244,7 @@ public:
     TErrorCode inline SetDisplayStartLine( std::uint8_t aDisplayStartLine ) NOEXCEPT
     {
         constexpr std::uint8_t KCmdSetDisplayStartLine = 0x40;
-        assert( aDisplayStartLine & 0x3F == aDisplayStartLine );
+        assert( aDisplayStartLine <= 0x3F );
         return SendCommand(
             static_cast< std::uint8_t >( KCmdSetDisplayStartLine | aDisplayStartLine & 0x3F ) );
     }
@@ -256,6 +254,134 @@ public:
         constexpr std::uint8_t KSegmentRemapDisabled = 0xA0;  // Column address 0 is mapped to SEG0
         constexpr std::uint8_t KSegmentRemapEnabled = 0xA1;  // Column address 127 is mapped to SEG0
         return SendCommand( aSegmentRemapEnabled ? KSegmentRemapEnabled : KSegmentRemapDisabled );
+    }
+
+    TErrorCode
+    SetMultiplexRatio( std::uint8_t aMultiplexRatio ) NOEXCEPT
+    {
+        using namespace AbstractPlatform;
+        constexpr std::uint8_t KCmdSetMultiplexRatio = 0xA8;
+
+        assert( aMultiplexRatio <= 0x3F );
+
+        const std::uint8_t commands[]
+            = { KCmdSetMultiplexRatio, static_cast< std::uint8_t >( aMultiplexRatio & 0x3F ) };
+
+        return SendCommands( commands, ArrayLength( commands ) );
+    }
+
+    TErrorCode inline SetCOMOutputScanDirection( bool aForwardDirectionScan = true ) NOEXCEPT
+    {
+        constexpr std::uint8_t KCmdForwardDirectionScan
+            = 0xC0;  // Normal mode (RESET) Scan from COM0 to COM[N –1], where N is the Multiplex
+                     // ratio.
+        constexpr std::uint8_t KCmdReverseDirectionScan
+            = 0xC8;  // Remapped mode. Scan from COM[N-1] to COM0], where N is the Multiplex ratio.
+        return SendCommand( aForwardDirectionScan ? KCmdForwardDirectionScan
+                                                  : KCmdReverseDirectionScan );
+    }
+
+    /**
+     * @brief Set vertical shift by COM from 0d~63d.
+     * The value is reset to 00h after RESET.
+     *
+     * @param aMultiplexRatio Vertical shift by COM from 0d~63d.
+     * @return TErrorCode KOk if succeed, otherwise appropriate error code
+     */
+    TErrorCode
+    SetDisplayOffset( std::uint8_t aDisplayOffset ) NOEXCEPT
+    {
+        using namespace AbstractPlatform;
+        constexpr std::uint8_t KCmdSetDisplayOffset = 0xD3;
+
+        assert( aDisplayOffset <= 0x3F );
+
+        const std::uint8_t commands[]
+            = { KCmdSetDisplayOffset, static_cast< std::uint8_t >( aDisplayOffset & 0x3F ) };
+
+        return SendCommands( commands, ArrayLength( commands ) );
+    }
+
+    TErrorCode
+    SetCOMPinsHardwareConfiguration( bool aAlternativeCOMPinConfiguration = true,
+                                     bool aEnableCOMLeftRightRemap = false ) NOEXCEPT
+    {
+        using namespace AbstractPlatform;
+        constexpr std::uint8_t KCmdSetCOMPinsHardwareConfiguration = 0xDA;
+        constexpr std::uint8_t KSequentialCOMPinConfiguration = 0x02;
+        constexpr std::uint8_t KAlternativeCOMpinConfiguration = 0x12;
+        constexpr std::uint8_t KDisableCOMLeftRightRemap = 0x02;
+        constexpr std::uint8_t KEnableCOMLeftRightRemap = 0x22;
+
+        const std::uint8_t commands[] = {
+            KCmdSetCOMPinsHardwareConfiguration,
+            static_cast< std::uint8_t >(
+                ( aEnableCOMLeftRightRemap ? KEnableCOMLeftRightRemap : KDisableCOMLeftRightRemap )
+                | ( aAlternativeCOMPinConfiguration ? KAlternativeCOMpinConfiguration
+                                                    : KSequentialCOMPinConfiguration ) ) };
+
+        return SendCommands( commands, ArrayLength( commands ) );
+    }
+
+    // Timing & Driving Scheme Setting Command
+    TErrorCode
+    SetDisplayClock( std::uint8_t aOscillatorFrequency = 0x08,
+                     std::uint8_t aDivideRatio = 1 ) NOEXCEPT
+    {
+        using namespace AbstractPlatform;
+        constexpr std::uint8_t KCmdSetDisplayClock = 0xD5;
+
+        assert( aDivideRatio <= 0x0F );
+        assert( aOscillatorFrequency <= 0x0F );
+        assert( aDivideRatio != 0x00 );
+
+        const std::uint8_t commands[] = {
+            KCmdSetDisplayClock, static_cast< std::uint8_t >( ( aOscillatorFrequency << 4 )
+                                                              | ( ( aDivideRatio - 1 ) & 0x0F ) ) };
+
+        return SendCommands( commands, ArrayLength( commands ) );
+    }
+
+    TErrorCode
+    SetPreChargePeriod( std::uint8_t aPhase1Period = 0x02,
+                        std::uint8_t aPhase2Period = 0x02 ) NOEXCEPT
+    {
+        using namespace AbstractPlatform;
+        constexpr std::uint8_t KCmdSetPreChargePeriod = 0xD9;
+
+        assert( aPhase1Period <= 0x0F );
+        assert( aPhase2Period <= 0x0F );
+
+        const std::uint8_t commands[]
+            = { KCmdSetPreChargePeriod,
+                static_cast< std::uint8_t >( ( aPhase2Period << 4 ) | ( aPhase1Period & 0x0F ) ) };
+
+        return SendCommands( commands, ArrayLength( commands ) );
+    }
+
+    enum VCOMHDeselectLevel
+    {
+        Level_0_65Vcc = 0x00,
+        Level_0_77Vcc = 0x20,  // (RESET)
+        Level_0_83Vcc = 0x30
+    };
+
+    TErrorCode
+    SetVCOMHDeselectLevel( VCOMHDeselectLevel aDeselectLevel = Level_0_77Vcc ) NOEXCEPT
+    {
+        using namespace AbstractPlatform;
+        constexpr std::uint8_t KCmdSetVCOMHDeselectLevel = 0xDB;
+
+        const std::uint8_t commands[]
+            = { KCmdSetVCOMHDeselectLevel, static_cast< std::uint8_t >( aDeselectLevel ) };
+
+        return SendCommands( commands, ArrayLength( commands ) );
+    }
+
+    TErrorCode inline Nop( ) NOEXCEPT
+    {
+        constexpr std::uint8_t KCmdNop = 0xE3;
+        return SendCommand( KCmdNop );
     }
 
     TErrorCode SendCommand( uint8_t aCommand, bool aNoStop = false ) NOEXCEPT;
